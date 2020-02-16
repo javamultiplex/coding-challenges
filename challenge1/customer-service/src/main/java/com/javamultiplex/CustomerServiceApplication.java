4fbdc9ef6fd1cd05430189e46c8fd016047c6f0d
Rohit Agarwal <programmingwithrohit@gmail.com>
2020-02-16 13:45:55 +0530

    Added service discovery in customer service

--- challenge1/customer-service/src/main/java/com/javamultiplex/CustomerServiceApplication.java
+++ challenge1/customer-service/src/main/java/com/javamultiplex/CustomerServiceApplication.java
@@ -2,6 +2,7 @@ package com.javamultiplex;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -19,6 +20,7 @@ public class CustomerServiceApplication {
     }
 
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
